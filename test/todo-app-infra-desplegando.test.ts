a0930a3f651891b6a1faa3d1d4b65ff7d35c5e12
Oscar Dario Florez Diaz <oscarflorez1381@gmail.com>
2025-09-29 14:11:27 -0400

    Initial commit

---
+++ test/todo-app-infra-desplegando.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TodoAppInfraDesplegando from '../lib/todo-app-infra-desplegando-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/todo-app-infra-desplegando-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TodoAppInfraDesplegando.TodoAppInfraDesplegandoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
